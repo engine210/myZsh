62d5ef6d20b5972a4738f765184c925884709561
Stevn Wang <engine210@gmail.com>
2020-08-06 01:44:26 +0800

    edit script

--- install.sh
+++ install.sh
@@ -21,7 +21,8 @@ git clone https://github.com/zsh-users/zsh-autosuggestions.git $HOME/.oh-my-zsh/
 # edit .zshrc ( commend here, use copy .zshrc instead )
 # sed -i 's/^ZSH_THEME=.*/ZSH_THEME="fishy"/g' ~/.zshrc
 # sed -i 's/^plugins=.*/plugins=(\n\tgit\n\tapple\n\tbanana\n)/g' ~/.zshrc
-cp .zshrc ~/.zshrc
+# backup .zshrc if already exist
+cp ~/myZsh/.zshrc ~/.zshrc
 
 # clean up
 rm -rf ~/myZsh
