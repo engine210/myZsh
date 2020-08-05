f7467d980db3e052fd2a9a51e3d662b5e905fa63
Stevn Wang <engine210@gmail.com>
2020-08-06 01:47:39 +0800

    edit script

--- install.sh
+++ install.sh
@@ -15,13 +15,18 @@ rm -rf ~/myZsh
 git clone https://github.com/engine210/myZsh.git ~/myZsh
 
 # install oh-my-zsh plugin
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.oh-my-zsh/custom/plugins/
-git clone https://github.com/zsh-users/zsh-autosuggestions.git $HOME/.oh-my-zsh/custom/plugins/
+cd $HOME/.oh-my-zsh/custom/plugins/
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
+git clone https://github.com/zsh-users/zsh-autosuggestions.git
+cd $HOME
 
 # edit .zshrc ( commend here, use copy .zshrc instead )
 # sed -i 's/^ZSH_THEME=.*/ZSH_THEME="fishy"/g' ~/.zshrc
 # sed -i 's/^plugins=.*/plugins=(\n\tgit\n\tapple\n\tbanana\n)/g' ~/.zshrc
 # backup .zshrc if already exist
+if [ -f $HOME/.zshrc ]; then
+	mv $HOME/.zshrc $HOME/.zshrc.bak.$(date "+%Y.%m.%d-%H:%M:%S")
+fi
 cp ~/myZsh/.zshrc ~/.zshrc
 
 # clean up
